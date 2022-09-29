ea044e87313a8cc613f2d47be4cd5a24f0c161ee
Thibault Leclercq <thibault@paradigme.tech>
2022-09-29 22:25:52 +0200

    default for tvshow release date

--- packages/api/src/modules/tmdb/tmdb.service.ts
+++ packages/api/src/modules/tmdb/tmdb.service.ts
@@ -28,6 +28,7 @@ import {
 import { CacheMethod } from '../redis/cache.interceptor';
 import { CacheKeys } from '../redis/cache.dto';
 import { RedisService } from '../redis/redis.service';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class TMDBService {
@@ -368,7 +369,7 @@ export class TMDBService {
       overview: result.overview,
       originalTitle: result.original_name,
       originCountry: result.origin_country,
-      releaseDate: result.first_air_date,
+      releaseDate: result.first_air_date ?? dayjs().year,
       posterPath: result.poster_path,
       voteAverage: result.vote_average,
     };
